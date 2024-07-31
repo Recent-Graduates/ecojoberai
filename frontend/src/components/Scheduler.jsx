83e417b58a717fbb4f161a249c0dc7db048f3c0a
Anu Shibin Joseph Raj <ajosephr@opentext.com>
2024-07-31 10:10:03 +0530

    🤡 Jobs & Machines

--- frontend/src/components/Scheduler.jsx
+++ frontend/src/components/Scheduler.jsx
@@ -10,10 +10,17 @@ export default function Scheduler() {
 					<option value="medium">Medium</option>
 					<option value="low">Low</option>
 				</select>
+				<label htmlFor="zone">Zone:</label>
+				<select id="zone" name="zone">
+					<option value="high">Zone A</option>
+					<option value="medium">Zone B</option>
+				</select>
 				<label htmlFor="deadline">Deadline:</label>
 				<input type="date" id="deadline" name="deadline" />
 				<label htmlFor="kind">Kind:</label>
 				<input type="text" id="kind" name="kind" />
+				<label htmlFor="distributable">Distributable:</label>
+				<input type="checkbox" id="distributable" name="distributable" />
 				<button type="submit">Add Job</button>
 			</form>
 			<h1>Jobs</h1>
@@ -22,8 +29,10 @@ export default function Scheduler() {
 					<tr>
 						<th>Job ID</th>
 						<th>Urgency</th>
+						<th>Originating zone</th>
 						<th>Deadline</th>
 						<th>Kind</th>
+						<th>Distributable</th>
 						<th>Status</th>
 					</tr>
 				</thead>
@@ -31,30 +40,66 @@ export default function Scheduler() {
 					<tr>
 						<td>1</td>
 						<td>High</td>
+						<td>Zone A</td>
 						<td>2024-06-15</td>
 						<td>Print</td>
+						<td>No</td>
 						<td>Scheduling in progress</td>
 					</tr>
 					<tr>
 						<td>2</td>
 						<td>Medium</td>
+						<td>Zone A</td>
 						<td>2024-06-20</td>
 						<td>Print</td>
+						<td>No</td>
 						<td>Not scheduled</td>
 					</tr>
 					<tr>
 						<td>3</td>
 						<td>Low</td>
+						<td>Zone B</td>
 						<td>2024-06-25</td>
 						<td>Print</td>
+						<td>No</td>
 						<td>Scheduled</td>
 					</tr>
 					<tr>
 						<td>4</td>
 						<td>Low</td>
+						<td>Zone B</td>
 						<td>2024-06-25</td>
 						<td>Print</td>
+						<td>No</td>
 						<td>Completed</td>
+					</tr>
+				</tbody>
+			</table>
+			<h1>Machines</h1>
+			<table border={1}>
+				<thead>
+					<tr>
+						<th>Machine ID</th>
+						<th>Name</th>
+						<th>Zone</th>
+						<th>Capabilities</th>
+						<th>Status</th>
+					</tr>
+				</thead>
+				<tbody>
+					<tr>
+						<td>1</td>
+						<td>Printer 1</td>
+						<td>Zone A</td>
+						<td>Print, Scan</td>
+						<td>Ready</td>
+					</tr>
+					<tr>
+						<td>2</td>
+						<td>Printer 2</td>
+						<td>Zone B</td>
+						<td>Print, Scan</td>
+						<td>Not Ready</td>
 					</tr>
 				</tbody>
 			</table>
