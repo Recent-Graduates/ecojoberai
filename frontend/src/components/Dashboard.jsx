da57968591aa301f1d2e8d2dd92339f60c0a0e99
Anu Shibin Joseph Raj <ajosephr@opentext.com>
2024-08-03 14:48:10 +0530

    💄 antd typography

--- frontend/src/components/Dashboard.jsx
+++ frontend/src/components/Dashboard.jsx
@@ -1,12 +1,11 @@
 import AllCharts from "./charts/AllCharts";
+import { Typography } from "antd";
+const { Title } = Typography;
 
 export default function Dashboard() {
 	return (
 		<>
-			<h1>Dashboard</h1>
-			<p>Dashboard is a place where you can see the data</p>
-			<h2>Job metrics</h2>
-			<p>Here you can see the job metrics as a graph</p>
+			<Title>Dashboard</Title>
 			<AllCharts />
 		</>
 	);
--- frontend/src/components/charts/AllCharts.jsx
+++ frontend/src/components/charts/AllCharts.jsx
@@ -1,3 +1,5 @@
+import { Typography } from "antd";
+const { Title } = Typography;
 import Chart from "react-apexcharts";
 import { chart1State, chart2State, chart3State } from "./ChartStates";
 import { Col, Row } from "antd";
@@ -13,9 +15,9 @@ export default function AllCharts() {
 		<>
 			<Row justify="space-evenly">
 				<Col xs={commonOptions.colXsSizeForCharts} md={commonOptions.colMdSizeForCharts}>
-					<h3>
+					<Title level={4}>
 						CO<sub>2</sub> consumption comparision
-					</h3>
+					</Title>
 					<Chart
 						options={chart2State.options}
 						series={chart2State.series}
@@ -25,9 +27,9 @@ export default function AllCharts() {
 					/>
 				</Col>
 				<Col xs={commonOptions.colXsSizeForCharts} md={commonOptions.colMdSizeForCharts}>
-					<h3>
+					<Title level={4}>
 						CO<sub>2</sub> saved per day
-					</h3>
+					</Title>
 					<Chart
 						options={chart3State.options}
 						series={chart3State.series}
@@ -37,7 +39,7 @@ export default function AllCharts() {
 					/>
 				</Col>
 				<Col xs={commonOptions.colXsSizeForCharts} md={commonOptions.colMdSizeForCharts}>
-					<h3>Print vs Scan Jobs</h3>
+					<Title level={4}>Print vs Scan Jobs</Title>
 					<Chart
 						options={chart1State.options}
 						series={chart1State.series}
