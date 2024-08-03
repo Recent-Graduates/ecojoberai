5a459c675115c0d620846f3fb611a5ef0f99e217
Anu Shibin Joseph Raj <ajosephr@opentext.com>
2024-08-03 15:50:58 +0530

    💄 Job Form Revamped

--- frontend/src/components/Scheduler.jsx
+++ frontend/src/components/Scheduler.jsx
@@ -2,32 +2,13 @@ import { Typography } from "antd";
 const { Title } = Typography;
 import JobsTable from "./jobs/JobsTable";
 import MachinesTables from "./machines/MachinesTable";
+import NewJob from "./jobs/NewJob";
 
 export default function Scheduler() {
 	return (
 		<>
 			<Title>Scheduler</Title>
-			<Title level={2}>Add a job</Title>
-			<form>
-				<label htmlFor="urgency">Urgency:</label>
-				<select id="urgency" name="urgency">
-					<option value="high">High</option>
-					<option value="medium">Medium</option>
-					<option value="low">Low</option>
-				</select>
-				<label htmlFor="zone">Zone:</label>
-				<select id="zone" name="zone">
-					<option value="high">Zone A</option>
-					<option value="medium">Zone B</option>
-				</select>
-				<label htmlFor="deadline">Deadline:</label>
-				<input type="date" id="deadline" name="deadline" />
-				<label htmlFor="kind">Kind:</label>
-				<input type="text" id="kind" name="kind" />
-				<label htmlFor="distributable">Distributable:</label>
-				<input type="checkbox" id="distributable" name="distributable" />
-				<button type="submit">Add Job</button>
-			</form>
+			<NewJob />
 			<JobsTable />
 			<MachinesTables />
 		</>
---
+++ frontend/src/components/jobs/NewJob.jsx
@@ -0,0 +1,89 @@
+import { Button, Collapse, Form, Input, Select, Switch } from "antd";
+const { Option } = Select;
+const formItemLayout = {
+	labelCol: {
+		xs: {
+			span: 24,
+		},
+		sm: {
+			span: 8,
+		},
+	},
+	wrapperCol: {
+		xs: {
+			span: 24,
+		},
+		sm: {
+			span: 16,
+		},
+	},
+};
+const tailFormItemLayout = {
+	wrapperCol: {
+		xs: {
+			span: 24,
+			offset: 0,
+		},
+		sm: {
+			span: 16,
+			offset: 8,
+		},
+	},
+};
+const commonRules = {
+	rules: [
+		{
+			required: true,
+		},
+	],
+};
+export default function NewJob() {
+	const onFinish = (values) => {
+		console.log("Received values of form: ", values);
+	};
+	const jobForm = (
+		<>
+			<Form
+				name="addjob"
+				{...formItemLayout}
+				onFinish={onFinish}
+				initialValues={{ distributable: false }}
+			>
+				<Form.Item label="Urgency" name="urgency" {...commonRules}>
+					<Select placeholder="Please select an Urgency">
+						<Option value="low">Low</Option>
+						<Option value="medium">Medium</Option>
+						<Option value="high">High</Option>
+					</Select>
+				</Form.Item>
+				<Form.Item label="Zone" name="zone" {...commonRules}>
+					<Select placeholder="Please select a Zone">
+						<Option value="a">Zone A</Option>
+						<Option value="b">Zone B</Option>
+					</Select>
+				</Form.Item>
+				<Form.Item label="Deadline" name="deadline" {...commonRules}>
+					<Input type="date" />
+				</Form.Item>
+				<Form.Item label="Kind" name="kind" {...commonRules}>
+					<Select placeholder="Please select a Job Kind">
+						<Option value="print">Print</Option>
+					</Select>
+				</Form.Item>
+				<Form.Item name="distributable" label="Distributable" {...commonRules}>
+					<Switch />
+				</Form.Item>
+				<Form.Item {...tailFormItemLayout}>
+					<Button type="primary" htmlType="submit">
+						Add Job
+					</Button>
+				</Form.Item>
+			</Form>
+		</>
+	);
+	return (
+		<>
+			<Collapse items={[{ key: "1", label: "Add a new job", children: jobForm }]} />
+		</>
+	);
+}
