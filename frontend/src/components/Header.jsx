82706894d29a1abb5122d498cebf5a61bd961695
Anu Shibin Joseph Raj <ajosephr@opentext.com>
2024-08-03 16:09:20 +0530

    💄 Better navbar

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,10 +1,24 @@
 import { Link } from "react-router-dom";
 import Constants from "../util/Constants";
+import { Menu } from "antd";
+const items = [
+	{
+		key: "home",
+		label: (
+			<>
+				<Link to={`${Constants.BASE_PATH}/`}>Home</Link>
+			</>
+		),
+	},
+	{
+		key: "scheduler",
+		label: (
+			<>
+				<Link to={`${Constants.BASE_PATH}/scheduler`}>Scheduler</Link>
+			</>
+		),
+	},
+];
 export default function Header() {
-	return (
-		<>
-			<Link to={`${Constants.BASE_PATH}/`}>Home</Link>
-			<Link to={`${Constants.BASE_PATH}/scheduler`}>Scheduler</Link>
-		</>
-	);
+	return <Menu items={items} mode="horizontal" />;
 }
