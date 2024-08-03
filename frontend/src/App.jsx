70d6de481d39de1cb836bfc6eb81749daaece4a5
Anu Shibin Joseph Raj <ajosephr@opentext.com>
2024-08-03 15:56:24 +0530

    ✨ ToastContainer

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,12 +2,26 @@ import { Outlet } from "react-router-dom";
 import "./App.css";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 function App() {
 	return (
 		<>
 			<Header />
 			<Outlet />
+			<ToastContainer
+				position="top-right"
+				autoClose={5000}
+				hideProgressBar={false}
+				newestOnTop={false}
+				closeOnClick
+				rtl={false}
+				pauseOnFocusLoss
+				draggable
+				pauseOnHover
+				theme="light"
+			/>
 			<Footer />
 		</>
 	);
