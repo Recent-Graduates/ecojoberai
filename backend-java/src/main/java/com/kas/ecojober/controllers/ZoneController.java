783648819e39bea8596385853cc91e6daef2f0e7
Satya Surya Aravinda Sarma Kodamanchili <karavindasar@opentext.com>
2024-08-04 19:24:24 +0530

    Fixed the zone controller by moving bussiness logic to service class

--- backend-java/src/main/java/com/kas/ecojober/controllers/ZoneController.java
+++ backend-java/src/main/java/com/kas/ecojober/controllers/ZoneController.java
@@ -1,8 +1,5 @@
 package com.kas.ecojober.controllers;
 
-import java.net.URI;
-
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
@@ -10,26 +7,22 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.client.RestTemplate;
-import org.springframework.web.util.UriComponentsBuilder;
+
+import com.kas.ecojober.services.ZoneService;
 
 @RestController
 @RequestMapping("/api/v1")
 public class ZoneController {
 
-	@Value("${electrycityMapsURL.url}")
-	private String electrycityMapsURL;
+	private ZoneService zoneService;
 
-	private final RestTemplate restTemplate;
-
-	public ZoneController(RestTemplate restTemplate) {
-		this.restTemplate = restTemplate;
+	public ZoneController(ZoneService zoneService) {
+		this.zoneService = zoneService;
 	}
 
 	@GetMapping("/zones")
 	public ResponseEntity<String> getAllZones() {
-		URI uri = UriComponentsBuilder.fromUriString(electrycityMapsURL).pathSegment("zones").build().toUri();
-		String response = restTemplate.getForObject(uri, String.class);
+		String response = zoneService.getAllZones();
 		HttpHeaders headers = new HttpHeaders();
 		headers.setContentType(MediaType.APPLICATION_JSON);
 		return new ResponseEntity<>(response, headers, HttpStatus.OK);
--- backend-java/src/main/java/com/kas/ecojober/services/ZoneService.java
+++ backend-java/src/main/java/com/kas/ecojober/services/ZoneService.java
@@ -1,5 +1,28 @@
 package com.kas.ecojober.services;
 
+import java.net.URI;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponentsBuilder;
+
+@Service
 public class ZoneService {
+
+	@Value("${electrycityMapsURL.url}")
+	private String electrycityMapsURL;
+
+	private final RestTemplate restTemplate;
+
+	public ZoneService(RestTemplate restTemplate) {
+		this.restTemplate = restTemplate;
+	}
+
+	public String getAllZones() {
+		URI uri = UriComponentsBuilder.fromUriString(electrycityMapsURL).pathSegment("zones").build().toUri();
+		String response = restTemplate.getForObject(uri, String.class);
+		return response;
+	}
 
 }
