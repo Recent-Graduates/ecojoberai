7cabc015995b60fc30bca9d1d02c0c9cc9358af8
Satya Surya Aravinda Sarma Kodamanchili <karavindasar@opentext.com>
2024-08-10 14:09:42 +0530

    SonorLint based fix

--- backend-java/src/main/java/com/kas/ecojober/configuration/RestTemplateConfig.java
+++ backend-java/src/main/java/com/kas/ecojober/configuration/RestTemplateConfig.java
@@ -8,7 +8,7 @@ import org.springframework.web.client.RestTemplate;
 public class RestTemplateConfig {
 
 	@Bean
-	public RestTemplate restTemplate() {
+	RestTemplate restTemplate() {
 		return new RestTemplate();
 	}
 
